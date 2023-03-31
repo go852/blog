feb86a767d8208f69c92aa4a5319e8e06be253d8
852us <paul.sunspipe@gmail.com>
2023-03-31 23:29:24 +0800

    Updated install.sh and removed uninstall.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "
+Usage:
+    $0 i | install: install modules
+    $0 u | uninstall: uninstall modules
+"
+    exit
+fi
+
 npm config set registry https://registry.npm.taobao.org 
 
 modules="
-  hexo-generator-archive
-  hexo-generator-category
-  hexo-generator-index
-  hexo-generator-tag
-  hexo-renderer-ejs
-  hexo-renderer-stylus
-  hexo-server
-  hexo-theme-landscape
-  hexo
   hexo-theme-stellar 
   hexo-filter-mathjax 
   hexo-renderer-pandoc 
@@ -19,6 +20,14 @@ modules="
 "
 
 for module in $modules; do
-  echo npm install $module --save
-  npm install $module --save
+  case $1 in
+  i | install )
+    echo npm install $module --save
+    npm install $module --save
+    ;;
+  u | uninstall )
+    echo npm uninstall $module --save
+    npm uninstall $module --save
+  ;;
+  esac
 done
