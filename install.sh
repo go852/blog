af31afe144999fee733d47b9cd06efba31382999
852us <paul.sunspipe@gmail.com>
2023-04-03 10:31:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,19 +17,15 @@ fi
 
 # npm config set registry https://registry.npm.taobao.org/
 
-# hexo-generator-search: 本地搜索需要安装此插件
-modules="
-  hexo-deployer-git
-  hexo-deployer-rsync
-  hexo-filter-mathjax 
-  hexo-generator-search
-  hexo-renderer-pandoc 
-  hexo-renderer-pug
-  hexo-renderer-stylus
-  hexo-theme-butterfly
-  hexo-theme-stellar 
-"
-
+# hexo-generator-search: 本地搜索
+# hexo-deployer-git: 一键部署到github服务器
+# hexo-deployer-rsync: 一键部署到ssh服务器
+# hexo-filter-mathjax: 数学公式解析
+# hexo-renderer-pandoc: 渲染
+# hexo-renderer-pug: butterfly主题渲染
+# hexo-renderer-stylus
+# hexo-theme-butterfly
+# hexo-theme-stellar 
 if [ -f package.json ] ; then
   modules=$(awk -F '[":]' '/^.*"hexo.*":/{print $2}' package.json)
 fi
